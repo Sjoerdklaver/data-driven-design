39400aec17a7c8193840ac846577184616fb4c27
Sjoerdklaver <43205388+Sjoerdklaver@users.noreply.github.com>
2018-10-03 10:56:38 +0200

    Add files via upload

---
+++ sentence_slicer.py
@@ -0,0 +1,28 @@
+"""
+sentence = input("Give me a sentence! ")
+
+sentence_length = len(sentence)
+sentence_start = sentence_length * 0.25
+sentence_end = sentence_length * 0.75
+sentence_start = round(sentence_start)
+sentence_end = round(sentence_end)
+
+print(sentence[sentence_start:sentence_end])
+"""
+
+#ADVANCED
+
+sentence = input("Give me a sentence! ")
+
+
+sentence_split = sentence.split()
+sentence_length = len(sentence_split)
+
+
+sentence_start = sentence_length * 0.25
+sentence_end = sentence_length * 0.75
+sentence_start = round(sentence_start)
+sentence_end = round(sentence_end)
+
+print(sentence_split[sentence_start:sentence_end])
+
